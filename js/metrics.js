0fdd6939ecff476437b98d8e49c0a82e5f9ed816
Manish Kumar Mourya <manish.mourya@theninehertzindia.com>
2024-08-06 16:13:10 +0530

    a

--- js/metrics.js
+++ js/metrics.js
@@ -1,5 +1,5 @@
 function collectMetrics() {
-    console.log(" performance.timing", performance)
+   // console.log(" performance.timing", performance)
   const metrics = {
     timing: performance.timing || 'N/A',
     navigation: performance.navigation || 'N/A',
@@ -9,6 +9,15 @@ function collectMetrics() {
   console.log('Collecting metrics:', metrics); // Debugging line to check metrics
 
   fetch('http://3.109.124.167:3000/metrics', { // Use the correct server IP and port
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(metrics)
+  }).catch(err => console.error('Failed to send metrics:', err));
+}
+
+fetch('http://3.109.124.167:3001/metrics', { // Use the correct server IP and port
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
